3bdb346da639ebb48b00a7783a4199cd3e260062
jasonleehodges <jasonleehodges@outlook.com>
2018-03-02 07:32:42 -0700

    plus

--- private/app.js
+++ private/app.js
@@ -1,6 +1,34 @@
 // babel private/app.js --out-file static/js/app.js --presets=es2015,react --watch
 
-const template = <p>Testing Testing 123</p>;
-const element = document.getElementById("main");
+let favs = 0;
+const addOne = () => {
+    favs++;
+    render();
+};
+const subOne = () => {
+    favs--;
+    render();
+};
+let dataset_columns = ["Month", "Sales", "Reps", "RepIDs"];
+let jsx_columns = dataset_columns.map((column) => {
+    return <li key={column}>{column}</li>;
+})
 
-ReactDOM.render(template, element);
+const render = () => {
+    const template = (
+        <div>
+            <p>React template!</p>
+            <p>Favorites: {favs}</p>
+            <button onClick={addOne}>+1</button>
+            <button onClick={subOne}>-1</button>
+            <ul>
+                {jsx_columns}
+            </ul>
+        </div>
+    );
+    const element = document.getElementById("main");
+
+    ReactDOM.render(template, element);
+};
+
+render();
--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,58 @@
 
 // babel private/app.js --out-file static/js/app.js --presets=es2015,react --watch
 
-var template = React.createElement(
-  "p",
-  null,
-  "Testing Testing 123"
-);
-var element = document.getElementById("main");
+var favs = 0;
+var addOne = function addOne() {
+    favs++;
+    render();
+};
+var subOne = function subOne() {
+    favs--;
+    render();
+};
+var dataset_columns = ["Month", "Sales", "Reps", "RepIDs"];
+var jsx_columns = dataset_columns.map(function (column) {
+    return React.createElement(
+        "li",
+        { key: column },
+        column
+    );
+});
 
-ReactDOM.render(template, element);
+var render = function render() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "p",
+            null,
+            "React template!"
+        ),
+        React.createElement(
+            "p",
+            null,
+            "Favorites: ",
+            favs
+        ),
+        React.createElement(
+            "button",
+            { onClick: addOne },
+            "+1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: subOne },
+            "-1"
+        ),
+        React.createElement(
+            "ul",
+            null,
+            jsx_columns
+        )
+    );
+    var element = document.getElementById("main");
+
+    ReactDOM.render(template, element);
+};
+
+render();
